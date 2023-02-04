ec7335db38b2312691daf3413b5b13ab536e6e12
magda <magda@LAPTOP-JBFKPILU>
2023-02-04 18:41:11 +0100

    Praca dom dzień 5 - cyfry

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,39 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-String name = "Magdalena";
-int age = 47;
-bool woman = true;
+int number = 221115377;
+int[] arrayOfSumsOfDigits = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int iterator;
 
-if (woman)
+string numberToString = number.ToString();//Console.WriteLine(numberToString);
+char[] arrayOfLetters = numberToString.ToCharArray();//Console.WriteLine(arrayOfLetters.Length);
+
+for(iterator=0; iterator< arrayOfLetters.Length; iterator++)
 {
-    if (age < 30)
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat.");       
-    }
-    else//ma co najmniej ma 30 lat
-    {
-        if (name == "Ewa" && age == 33)
-        {
-            Console.WriteLine("Ewa, lat 33.");
-        }
-        else // nie Ewa i nie ma 33 lat
-        {
-            Console.WriteLine("Nazywam się " + name + " i mam lat " + age + ".");
-        }
-    }
-}
-else //mezczyzna
-{
-    if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna.");
-    }
-    else
-    {
-        Console.WriteLine("Mężczyzna jest pełnoletni..");
-    }    
+    int digitFromChar = (int)arrayOfLetters[iterator];
+    arrayOfSumsOfDigits[digitFromChar - 48]++;
 }
 
-
-
+for (iterator = 0; iterator < 10; iterator++) 
+{
+    Console.WriteLine(iterator + " => " + arrayOfSumsOfDigits[iterator]);
+}
+    
 
