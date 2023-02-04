436c41393b4f46139ed174d64a1bca66d75fcd91
magda <magda@LAPTOP-JBFKPILU>
2023-02-04 11:01:38 +0100

    Praca domowa dzień 4 (poprawione)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,31 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-String Name = "Magdalena";
-int Age = 47;
-bool Woman = true;
+String name = "Magdalena";
+int age = 47;
+bool woman = true;
 
 //zaraz chwila prawdy
-if (Woman == true)
+if (woman)
 {
-    if (Age < 30)
+    if (age < 30)
     {
         Console.WriteLine("Kobieta poniżej 30 lat.");       
     }
     else//ma co najmniej ma 30 lat
     {
-        if (Name == "Ewa" && Age == 33)
+        if (name == "Ewa" && age == 33)
         {
             Console.WriteLine("Ewa, lat 33.");
         }
         else // nie Ewa i nie ma 33 lat
         {
-            Console.WriteLine("Nazywam się " + Name + " i mam lat " + Age + ".");
+            Console.WriteLine("Nazywam się " + name + " i mam lat " + age + ".");
         }
     }
 }
 else //mezczyzna
 {
-    if (Age < 18)
+    if (age < 18)
     {
         Console.WriteLine("Niepełnoletni mężczyzna.");
     }
