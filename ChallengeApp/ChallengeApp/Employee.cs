4126c22e1a34213b45e5ae4112dbccc378926a37
magda <magda@LAPTOP-JBFKPILU>
2023-02-06 11:07:45 +0100

    Dzien 6 - Klasy

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace ChallengeApp
+{
+    class Employee
+    {
+        //private String firstName, lastName;
+        //private int age;
+        private List<int> scores;
+
+
+        public String firstName { get; private set; }
+        public String lastName { get; private set; }
+        public int age { get; private set; }
+
+        //public int result { get { return this.scores.Sum(); } };
+
+        public Employee(String firstName, String lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.scores = new List<int>();
+        }
+
+
+
+        public void addMarks(int[] points)
+        {
+            for(int i=0;i<points.Length;i++)
+                this.scores.Add(points[i]);
+        }
+        public int getMaxScore()
+        {
+            return this.scores.Max();
+        }
+        public int getSumOfScores()
+        {
+            return this.scores.Sum();
+        }
+
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,45 @@
-﻿
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-int number = 221115377;
-int[] arrayOfSumsOfDigits = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-int iterator;
+using ChallengeApp;
+//utworzenie listy na pracownikó
+List<Employee> listOfEmpls = new List<Employee>();
+//dodanie pracownikow
+listOfEmpls.Add(new Employee("Magdalena", "Pejas", 47));
+listOfEmpls.Add(new Employee("Adam", "Kamiz", 30));
+listOfEmpls.Add(new Employee("Zuzanna", "Xinska", 35));
 
-string numberToString = number.ToString();//Console.WriteLine(numberToString);
-char[] arrayOfLetters = numberToString.ToCharArray();//Console.WriteLine(arrayOfLetters.Length);
+//dodanie kazdemu ocen
+listOfEmpls[0].addMarks(new int[5] {2,5,3,6,4});
+listOfEmpls[1].addMarks(new int[5] {5,2,9,8,2});
+listOfEmpls[2].addMarks(new int[5] {4,2,7,5,3});
 
-for(iterator=0; iterator< arrayOfLetters.Length; iterator++)
+//szukaniepracownika o najwyzszej sumie ocen
+int maxResult = -1;
+Employee theChampion = null;
+foreach (var oneEmpl in listOfEmpls)
 {
-    int digitFromChar = (int)arrayOfLetters[iterator];
-    arrayOfSumsOfDigits[digitFromChar - 48]++;
+    //Console.WriteLine(oneEmpl.firstName + oneEmpl.getSumOfScores());
+    int thisScore = oneEmpl.getSumOfScores();
+    if (thisScore > maxResult)
+    {
+        theChampion = oneEmpl;
+        maxResult = thisScore;
+    }
+}
+//wypisanie danych pracownika o najlepszym wyniku (sumie ocen)
+if (theChampion !=null)
+{
+    Console.WriteLine("Najlepszy: " + theChampion.firstName + " " + theChampion.lastName);
+    Console.WriteLine("Wiek: " + theChampion.age);
+    Console.WriteLine("Punkty - suma: " + theChampion.getSumOfScores());
 }
 
-for (iterator = 0; iterator < 10; iterator++) 
-{
-    Console.WriteLine(iterator + " => " + arrayOfSumsOfDigits[iterator]);
-}
-    
+
+
+
+
 
